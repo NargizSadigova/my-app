7353444bfdb39839474cc4ceaa6310009e3af5fd
NargizSadigova <nargizsadigova1304@gmail.com>
2024-12-10 16:58:44 +0400

    portfolio

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ import Contact from './Component/Contact';
 function App() {
   return (
     <div className="App">
-     
-      {/* <div className='menu'>
-      <Link to='/'>About</Link>
-      <Link to='/portfolio'>Portfolio</Link>
-      <Link to='/contact'>Contact</Link>
-      </div> */}
+    
       <div className='menu'>
       <NavLink to='/'>About</NavLink>
       <NavLink to='/portfolio'>Portfolio</NavLink>
